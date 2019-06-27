6d70bfcd9a1e943d84b0c7b98c56afc5215394a0
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-06-26 19:14:31 -0500

    github connection testing

--- github-requests.js
+++ github-requests.js
@@ -1,3 +1,5 @@
-
-
-var data = get
+const data = d3.json(`https://api.github.com/repos/AvidDabbler/${name}`).then(function(data){
+        console.log(data);
+        console.log(data['full_name']);
+        attGen("walterkjenkins");
+        });
--- main.js
+++ main.js
@@ -1,20 +1,57 @@
-const repoList = [
+const repos = [
     "walterkjenkins",
     "censusViz",
     "canihazchix"
 ];
+
+
+const datas = function (name){
+    d3.json(`https://api.github.com/repos/AvidDabbler/${name}`).then(function(name){
+        const urlGen = `https://api.github.com/repos/AvidDabbler/${name}`;
+        const thumbnailGen = `https://raw.githubusercontent.com/AvidDabbler/${name}/thumbnail.PNG`;
+        const aboutGen = datas['description']; // github api repo object
+        
+        return{
+            url: urlGen,
+            thumbnail: thumbnailGen,
+            about: aboutGen,
+    
+        }
+
+        // console.log(data);
+        // console.log(data['full_name']);
+        // console.log(attGen(name));
+        })
+  
+    }
+
+// class Attributes{
+//     constructor(name, url, thumbnail, about, tags){
+//         this.name = name,
+//         this.url = url,
+//         this.thumbnail = thumbnail,
+//         this.about = about,
+//         this.tags = tags
+//     }
+
+// };
+
+
+console.log(datas('CensusViz')('CensusViz'));
+
+
+
 
 // create a separte object that extends the repoList
 // url, fullName, profilePic, about, tags
 
 // https://raw.githubusercontent.com/AvidDabbler/walterkjenkins/master/assets/images/ArcGIS-Pro.png
 
-const repoAttr = ;
 
 
-const data = d3.json("https://api.github.com/repos/AvidDabbler/walterkjenkins").then(function(data){
-    console.log(data);
-    console.log(data['full_name']);
+// const data = d3.json(`https://api.github.com/repos/AvidDabbler/${name}`).then(function(data){
+//     console.log(data);
+//     console.log(data['full_name']);
 
-});
+// });
 
