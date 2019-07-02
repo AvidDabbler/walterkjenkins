ae1435388c018397e55c75ec195364b1498a108b
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-07-02 16:28:16 -0500

    update to constructor

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 class repoConstr {
     constructor(repo){
         this.repo = repo;
-        this.url = `https://api.github.com/repos/AvidDabbler/${repo}`;
-        this.thumbnail = `https://raw.githubusercontent.com/AvidDabbler/${repo}/thumbnail.png`;
-        this.about = (repo) => { 
-            const makeUrl = `https://api.github.com/repos/AvidDabbler/${repo}`;   
-            const partone = fetch(makeUrl).then(response=> response.json());
-            partone.then(myJson =>JSON.stringify(myJson));
+        this.apiUrl = `https://api.github.com/repos/AvidDabbler/${repo}`;
+        this.gitUrl = `https://github.com/AvidDabbler/${repo}`;
+        this.thumbnail = `https://raw.githubusercontent.com/AvidDabbler/${repo}/thumbnail.PNG`;
+        this.about = () => { 
+            const partone = fetch(u).then(response=> response.json());
+            partone(this.url);
+            const data = partone.then(myJson =>JSON.stringify(myJson));
+            return data;
            }
     }
 }
@@ -16,3 +18,13 @@ const repos = [
     new repoConstr(`censusViz`),
     new repoConstr(`canihazchix`)
 ];
+
+console.log(repos);
+
+
+const getdata = function() { 
+    const partone = fetch(`https://api.github.com/repos/AvidDabbler/walterkjenkins`).then(response=> response.json());
+    partone.then(myJson =>JSON.stringify(myJson));
+}
+
+console.log(getdata);
