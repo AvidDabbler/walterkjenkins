993476a642f3fd75cd67c4bcea6ad8cf6acc077c
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-07-14 10:05:34 -0500

    fixed pending data issue and constructor functions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const description = async (link)=>{
+const getData = async(id, repo)=>{
+    const link = `https://api.github.com/repos/AvidDabbler/${repo}`;
     const fetchedData = fetch(link).then(response=>
         response.json()).then(myJson=> ({
             data:myJson
@@ -6,64 +7,31 @@ const description = async (link)=>{
         ).then(res =>{
             return res.data.description
         });
-    return fetchedData
-};
+    return {
+            id: id,
+            repo: repo,
+            gitUrl: `https://github.com/AvidDabbler/${repo}`,
+            thumbnail: `https://raw.githubusercontent.com/AvidDabbler/${repo}/master/thumbnail.PNG`,
+            description: await fetchedData
+            }
+        };
 
 
-class repoConstr {
-    constructor(id, repo){
-        this.id = id;
-        this.repo = repo;
-        const apiurldata = await fetch(`https://api.github.com/repos/AvidDabbler/${repo}`);
-        this.apiUrl = apiurldata;
-        this.gitUrl = `https://github.com/AvidDabbler/${repo}`;
-        this.thumbnail = `https://raw.githubusercontent.com/AvidDabbler/${repo}/master/thumbnail.PNG`;
-        this.description = description(this.apiUrl);
-    }
-/*     description(){
-        fetch(this.apiUrl).then(response=>
-            response.json()).then(myJson=> ({
-                data:myJson
-            })
-            ).then(res =>{
-                const data = res.data.description;
-            });
-        return data;
-    } */
 
-}
-
-const repos = [
-    new repoConstr(1, `walterkjenkins`),
-    new repoConstr(2, `censusViz`),
-    new repoConstr(3, `backyard-chickens`),
-    new repoConstr(4, `dlm-soundboard`)
+/* DEFINED REPOS TO DISPLAY */
+const setupArr = [
+    [0, `censusViz`],
+    [1, `backyard-chickens`],
+    [2, `dlm-soundboard`]
 ];
 
-const reposFunction = async () => {
-    const setupArr = [
-        [1, `walterkjenkins`],
-        [2, `censusViz`],
-        [3, `backyard-chickens`],
-        [4, `dlm-soundboard`]
-    ];
-    setupArr.forEach
-    function getData(){
+const reposFunction = () => {
+    setupArr.forEach(async(el)=>{
+        const getDataObj = await getData(el[1], el[1]);
+        console.log(getDataObj);
+    });
 
-    }
 };
 
-const test = new repoConstr(1, 'walterkjenkins');
+reposFunction();
 
-
-//this works but runs before test.description() can run
-console.log(test);
-
-
-
-
-/* const renderPortfolio = (()=>{
-    data.map(data.repo);
-});
-
-console.log(renderPortfolio(data)); */
