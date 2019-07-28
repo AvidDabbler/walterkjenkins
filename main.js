0edcfa0dcf1166c5471de6fdc6eaef3e97ae65e3
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-07-28 15:37:11 -0500

    portfRender() updates

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+/**
+ * TODO: GET PORTRENDER() FOR LOOP TO RETURN SOMETHING (ASYNC ERROR?)
+ * TODO: ADD IN THE FOLLOWING -> document.getElementById("portfolio-content").innerHTML+= `<div id="project-${i}" class="portfolio-container content"><div class="thumbnail"><a href="${data[i].homepage}" target="_blank"><img class="" src="${data[i].thumbnail}" alt="${data[i].title} application photo"></img></a></div> <div class="paragraph"><h2>${data[i].title}</h2><p>${data[i].descr}</p></div>`
+ */
+
+
+//! DEFINING DEFAULTS
+
 const user = `AvidDabbler`; /* ENTER IN GITHUB USERNAME */
-const split = ` || `;
+const split = ` || `;/*  DEFAULT SEPARATOR */
 const finalArr= [];
 
 
-const getData = async(repo, usr = user, splt = split)=>{
+// ! FETCHING AND BUILDING GITHUB DATA OBJECTS
+
+const getData = async(i, repo, usr = user, splt = split)=>{
     const gitLink = `https://api.github.com/repos/${usr}/${repo}`;
     const fetchedData = fetch(gitLink).then( response=> 
         response.json()).then(myJson=> ({
@@ -28,15 +38,14 @@ const getData = async(repo, usr = user, splt = split)=>{
     const formTitle = objArr[0];
     const description = objArr[1];
     const technology = objArr[2].split(', ');
-    const count = finalArr.length;
     const particData = await statData;
 
 
     return {
-            id: count,
+            id: i,
             repo: repo,
             gitUrl: `https://github.com/${usr}/${repo}`,
-            thumbnail: `https://raw.githubusercontent.com/${usr}/${repo}/master/thumbnail.PNG`,
+            thumbnail: `https://raw.githubusercontent.com/${usr}/${repo}/master/thumbnail.png`,
             homepage: `https://${usr}.github.io/${repo}/`,
             apiUrl: gitLink,
             title: await formTitle,
@@ -46,21 +55,35 @@ const getData = async(repo, usr = user, splt = split)=>{
             }
         };
 
-/* DEFINED REPOS TO DISPLAY */
+
+// * DEFINED REPOS TO DISPLAY
 const setupArr = [
-    [`censusViz`],
+    [`CensusViz`],
     [`backyard-chickens`],
     [`dlm-soundboard`]
 ];
 
-const reposFunction = () => {
-    setupArr.forEach(async(el)=>{
-        const getDataObj = await getData(el[0], el[1], el[2], el[3]);
+
+// * CALLS ALL OF THE FUNCTIONS AND PUSHES INTO finalArr
+const reposFunction = async () => { 
+    for(let i = 0; i < setupArr.length; i++){
+        let el = setupArr[i];
+        const getDataObj = await getData(i, el[0], el[1], el[2]);/* el 2 and 3 are optional */
         finalArr.push(getDataObj);
-    });
-    return finalArr;
+    };
 };
 
-reposFunction();
-console.log(finalArr)
 
+// * GITHUB DATA RENDERING
+const portfGen= async()=>{ 
+    await reposFunction();
+    const data = await finalArr;
+    for(let i=0; i < data.length; i++){
+        let descr = await data[i];
+        console.log(descr); //! FOR LOOP DOES NOT RETURN ANYTHING
+        document.getElementById("portfolio-content").innerHTML+= `<div id="project-${i}" class="portfolio-container content"><div class="thumbnail"><a href="${data[i].homepage}" target="_blank"><img class="" src="${data[i].thumbnail}" alt="${data[i].title} application photo"></img></a></div> <div class="paragraph"><h2>${data[i].title}</h2><p>${data[i].descr}</p></div>`
+    }
+}
+
+
+portfGen();
